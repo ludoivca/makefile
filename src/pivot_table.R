f22d670d5d4ea4bde6867c5825e9c948f67e9150
Ludovica Donatelli <ludovica.donatelli@studenti.luiss.it>
2022-10-12 11:54:43 +0200

    move to source code and updated dependencies

---
+++ src/pivot_table.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(reshape2)
+library(magrittr)
+
+######################
+# CREATE PIVOT TABLE #
+######################
+
+# import the data from `gen/data-preparation/aggregated_df.csv`
+df <- read.csv("temp/aggregated_df.csv")
+
+# create pivot table
+df_pivot <- df %>% dcast(date ~ neighbourhood, fun.aggregate = sum, value.var = "num_reviews")
+
+# export results
+write.csv(df_pivot, "temp/pivot_table.csv")
+
+
